55fb5c1f63908514a2293725f155d89deb0ea3f2
patricioaguilera367-hash <patricio.aguilera367@gmail.com>
2026-09-18 15:37:05 -0300

    test(domain): specify drawing geometry and provenance

---
+++ tests/UI_Unilineal.Domain.Tests/Profiles/GraphicRuleSourceTests.cs
@@ -0,0 +1,51 @@
+﻿using UI_Unilineal.Domain.Profiles;
+
+namespace UI_Unilineal.Domain.Tests.Profiles;
+
+public sealed class GraphicRuleSourceTests
+{
+    [Fact]
+    public void Constructor_RejectsBlankId()
+    {
+        Assert.Throws<ArgumentException>(() => new GraphicRuleSource(
+            " ",
+            GraphicRuleClassification.APP_CONVENTION,
+            null,
+            null,
+            null,
+            null,
+            "Initial application drawing convention."));
+    }
+
+    [Fact]
+    public void Constructor_RejectsBlankDescription()
+    {
+        Assert.Throws<ArgumentException>(() => new GraphicRuleSource(
+            "APP:BASE",
+            GraphicRuleClassification.APP_CONVENTION,
+            null,
+            null,
+            null,
+            null,
+            " "));
+    }
+
+    [Fact]
+    public void AppConvention_DoesNotRequireRegulatoryCitation()
+    {
+        var source = new GraphicRuleSource(
+            "APP:BASE",
+            GraphicRuleClassification.APP_CONVENTION,
+            null,
+            null,
+            null,
+            null,
+            "Initial application drawing convention.");
+
+        Assert.Null(source.Document);
+        Assert.Null(source.Section);
+        Assert.Equal(
+            GraphicRuleClassification.APP_CONVENTION,
+            source.Classification);
+    }
+}
---
+++ tests/UI_Unilineal.Domain.Tests/Scene/GeometryTests.cs
@@ -0,0 +1,34 @@
+﻿using UI_Unilineal.Domain.Scene;
+
+namespace UI_Unilineal.Domain.Tests.Scene;
+
+public sealed class GeometryTests
+{
+    [Fact]
+    public void MmRect_RejectsNonPositiveSize()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => new MmRect(0, 0, 0, 10));
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => new MmRect(0, 0, 10, -1));
+    }
+
+    [Theory]
+    [InlineData(double.NaN, 0)]
+    [InlineData(double.PositiveInfinity, 0)]
+    [InlineData(0, double.NegativeInfinity)]
+    public void MmPoint_RejectsNonFiniteCoordinates(double x, double y)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => new MmPoint(x, y));
+    }
+
+    [Fact]
+    public void MmRect_ComputesRightAndBottom()
+    {
+        var rect = new MmRect(2.5, 3.5, 10, 20);
+
+        Assert.Equal(12.5, rect.Right);
+        Assert.Equal(23.5, rect.Bottom);
+    }
+}
