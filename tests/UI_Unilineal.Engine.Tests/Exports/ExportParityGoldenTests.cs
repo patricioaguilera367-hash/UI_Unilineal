716290f255cd6f73642ec61332050d718d857ab3
patricioaguilera367-hash <patricio.aguilera367@gmail.com>
2026-09-19 14:09:11 -0300

    fix(exports): resolve profile loader in parity RED

--- tests/UI_Unilineal.Engine.Tests/Exports/ExportParityGoldenTests.cs
+++ tests/UI_Unilineal.Engine.Tests/Exports/ExportParityGoldenTests.cs
@@ -5,6 +5,7 @@ using System.Xml.Linq;
 using UI_Unilineal.Domain.Profiles;
 using UI_Unilineal.Domain.Scene;
 using UI_Unilineal.Domain.Semantics;
+using UI_Unilineal.Engine.Composition;
 using UI_Unilineal.Engine.Documents;
 using UI_Unilineal.Engine.Layout;
 using UI_Unilineal.Engine.Projection;
