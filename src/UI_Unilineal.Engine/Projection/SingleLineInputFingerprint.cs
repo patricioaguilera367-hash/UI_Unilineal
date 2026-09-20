c2247221f1d596ced6586b177695d50cf395b641
patricioaguilera367-hash <patricio.aguilera367@gmail.com>
2026-09-20 00:16:57 -0300

    fix(projection): preserve legacy fingerprints when empalme data is absent

--- src/UI_Unilineal.Engine/Projection/SingleLineInputFingerprint.cs
+++ src/UI_Unilineal.Engine/Projection/SingleLineInputFingerprint.cs
@@ -11,40 +11,73 @@ public static class SingleLineInputFingerprint
     {
         ArgumentNullException.ThrowIfNull(input);
 
-        var normalized = new
-        {
-            input.Project,
-            Sources = input.Sources
-                .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
-                .ToArray(),
-            Boards = input.Boards
-                .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
-                .ToArray(),
-            Buses = input.Buses
-                .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
-                .ToArray(),
-            Circuits = input.Circuits
-                .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
-                .ToArray(),
-            SupplyConnections = input.SupplyConnections
-                .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
-                .ToArray(),
-            Protections = input.Protections
-                .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
-                .ToArray(),
-            Grounding = input.Grounding
-                .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
-                .ToArray(),
-            ServiceEntrances = input.ServiceEntrances
-                .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
-                .ToArray(),
-            Results = input.Results
-                .OrderBy(x => x.Entity.Kind)
-                .ThenBy(x => x.Entity.Uid.Value, StringComparer.Ordinal)
-                .ThenBy(CanonicalResultSortKey, StringComparer.Ordinal)
-                .ToArray(),
-            input.Metadata
-        };
+        object normalized =
+            input.ServiceEntrances.Count == 0
+                ? new
+                {
+                    input.Project,
+                    Sources = input.Sources
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Boards = input.Boards
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Buses = input.Buses
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Circuits = input.Circuits
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    SupplyConnections = input.SupplyConnections
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Protections = input.Protections
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Grounding = input.Grounding
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Results = input.Results
+                        .OrderBy(x => x.Entity.Kind)
+                        .ThenBy(x => x.Entity.Uid.Value, StringComparer.Ordinal)
+                        .ThenBy(CanonicalResultSortKey, StringComparer.Ordinal)
+                        .ToArray(),
+                    input.Metadata
+                }
+                : new
+                {
+                    input.Project,
+                    Sources = input.Sources
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Boards = input.Boards
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Buses = input.Buses
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Circuits = input.Circuits
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    SupplyConnections = input.SupplyConnections
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Protections = input.Protections
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Grounding = input.Grounding
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    ServiceEntrances = input.ServiceEntrances
+                        .OrderBy(x => x.Uid.Value, StringComparer.Ordinal)
+                        .ToArray(),
+                    Results = input.Results
+                        .OrderBy(x => x.Entity.Kind)
+                        .ThenBy(x => x.Entity.Uid.Value, StringComparer.Ordinal)
+                        .ThenBy(CanonicalResultSortKey, StringComparer.Ordinal)
+                        .ToArray(),
+                    input.Metadata
+                };
 
         string json = JsonSerializer.Serialize(normalized);
         byte[] bytes = Encoding.UTF8.GetBytes(json);
