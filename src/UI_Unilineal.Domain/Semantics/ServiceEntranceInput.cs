734376dedf278197ce87f9dc35371db6e8df7119
patricioaguilera367-hash <patricio.aguilera367@gmail.com>
2026-09-19 22:06:54 -0300

    feat(domain): model configurable service entrance and meter

---
+++ src/UI_Unilineal.Domain/Semantics/ServiceEntranceInput.cs
@@ -0,0 +1,27 @@
+﻿namespace UI_Unilineal.Domain.Semantics;
+
+public enum MeterKind
+{
+    SinglePhase,
+    ThreePhase,
+    Other,
+    Unknown
+}
+
+public enum InputValueAuthority
+{
+    Manual,
+    Suggested,
+    Computed
+}
+
+public sealed record ServiceEntranceInput(
+    EntityUid Uid,
+    EntityUid SourceUid,
+    MeterKind MeterKind,
+    string? TariffCode,
+    string? Details,
+    EntityUid? ProtectionUid,
+    InputValueAuthority ProtectionAuthority,
+    OperationalState State,
+    DataState DataState);
