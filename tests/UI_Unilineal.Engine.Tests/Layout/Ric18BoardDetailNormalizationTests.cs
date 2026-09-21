7f1fc6524e3f334a0624f1c2d7197f1a2cb855d3
patricioaguilera367-hash <patricio.aguilera367@gmail.com>
2026-09-20 21:33:13 -0300

    fix(test): import AnchorDirection namespace

--- tests/UI_Unilineal.Engine.Tests/Layout/Ric18BoardDetailNormalizationTests.cs
+++ tests/UI_Unilineal.Engine.Tests/Layout/Ric18BoardDetailNormalizationTests.cs
@@ -1,4 +1,5 @@
-﻿using UI_Unilineal.Domain.Profiles;
+﻿using UI_Unilineal.Domain.Connections;
+using UI_Unilineal.Domain.Profiles;
 using UI_Unilineal.Domain.Scene;
 using UI_Unilineal.Domain.Semantics;
 using UI_Unilineal.Engine.Composition;
